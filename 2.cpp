0ec29d30cec0a16bfd3c07a5086b6c57ae952398
CocoJamboOG <jeff41yt@gmail.com>
2023-10-30 11:59:35 +0100

    Komentarz

---
+++ 2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	
+	short a, b;
+	cout <<"podaj a:";
+	cin >> a;
+	cout <<"podaj b:";
+	cin >> b;
+	
+	if(a>b){
+		cout <<"a > b";
+	}
+	else{
+		cout <<"a < b";
+	}
+	
+	
+	
+	cin >> a
+	
+	return 0;
+}
