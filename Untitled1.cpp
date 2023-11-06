9a2f33d29d7246d7f68dc3e5d1449253ad87bdbf
CocoJamboOG <jeff41yt@gmail.com>
2023-11-06 11:59:14 +0100

    Komentarz

---
+++ Untitled1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	for(int i = 0; i < 5; i++){
+		cout << "Iteracj petli:" << i << endl;
+	}
+	
+	
+	for(int i = 0; i < 5; i++){
+		for(int j = 0; j < 5; j++){
+			cout << "i:" << i;
+			cout << "j:" << j;
+		}
+		cout << endl;
+	}
+	
+	
+	
+	
+
+	
+	cout << endl;
+	system("pause");
+}
